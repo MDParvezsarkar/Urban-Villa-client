64e86b610dacd64fba6105faa1380ab6a348170b
MDParvezsarkar <mdparvezsarkar10@gmail.com>
2025-07-15 17:01:49 +0600

    Made Mar Markar custom

--- src/Pages/Home/Location/Location.jsx
+++ src/Pages/Home/Location/Location.jsx
@@ -10,9 +10,31 @@ import {
   FaTrain,
 } from "react-icons/fa";
 import "leaflet/dist/leaflet.css";
+import L from "leaflet";
+
+//  Import Leaflet marker images (fixes broken icons)
+import markerIcon2x from "leaflet/dist/images/marker-icon-2x.png";
+import markerIcon from "leaflet/dist/images/marker-icon.png";
+import markerShadow from "leaflet/dist/images/marker-shadow.png";
+
+//  Fix default Leaflet icon issue
+delete L.Icon.Default.prototype._getIconUrl;
+L.Icon.Default.mergeOptions({
+  iconRetinaUrl: markerIcon2x,
+  iconUrl: markerIcon,
+  shadowUrl: markerShadow,
+});
+
+// custom icon instead 
+const customIcon = new L.Icon({
+  iconUrl: "https://i.ibb.co/0yCB63fG/marker-2.png",
+  iconSize: [32, 32],
+  iconAnchor: [16, 32],
+  popupAnchor: [0, -32],
+});
 
 const Location = () => {
-  const position = [23.8103, 90.4125];
+  const position = [23.8103, 90.4125]; 
 
   useEffect(() => {
     AOS.init({ duration: 1000 });
@@ -25,11 +47,11 @@ const Location = () => {
           className="text-4xl font-bold text-center mb-8 text-gray-800"
           data-aos="fade-up"
         >
-           Our Apartment Location
+          Our Apartment Location
         </h2>
 
         <div className="grid md:grid-cols-2 gap-10 items-center">
-          {/* Info */}
+          {/* Info Section */}
           <div data-aos="fade-right">
             <h3 className="text-2xl font-semibold mb-4 text-gray-700 flex items-center gap-2">
               <FaMapMarkedAlt className="text-secondary" /> How to Reach Us
@@ -57,7 +79,7 @@ const Location = () => {
             </ul>
           </div>
 
-          {/* Map */}
+          {/* Map Section */}
           <div
             className="rounded-xl overflow-hidden shadow-xl ring-2 ring-secondary hover:ring-primary transition duration-300 h-[300px] md:h-[400px]"
             data-aos="fade-left"
@@ -69,7 +91,7 @@ const Location = () => {
               className="h-full w-full z-0"
             >
               <TileLayer url="https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png" />
-              <Marker position={position}>
+              <Marker position={position} icon={customIcon}>
                 <Popup>
                   Our Apartment <br /> Visit us anytime!
                 </Popup>
